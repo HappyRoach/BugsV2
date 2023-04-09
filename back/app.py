7190edb9a78fd062a18869417b6f3c372524eaba
HappyRoach <117393350+HappyRoach@users.noreply.github.com>
2023-04-09 12:26:38 +0300

    JSON added

--- back/app.py
+++ back/app.py
@@ -1,19 +1,34 @@
-from flask import Flask, request, jsonify
-from flask_restful import Api
-app = Flask(__name__)
-api = Api(app)
 import SQL
+from flask import Flask, jsonify, request
+import json
 
-@app.route('/Bugs', methods=['GET'])
+app = Flask(__name__)
+
+@app.route('/DB', methods=['GET'])
 def GET():
     return SQL.Db().select_all_user()
 
-@app.route('/Bugs', methods=['POST'])
+@app.route('/DB', methods=['POST'])
 def POST(): #Не ебу как, но оно работает, хоть и выдает ошибку 500. Главное работает.
     SQL.Db().insert_user(request.json['fullname'], request.json['date_time'], request.json['pose'], request.json['curreet_pose'], request.json['offical_pose'], request.json['email'], request.json['data_work'], request.json['region'], request.json['phone'], request.json['work_time'], request.json['spec'])
 
-@app.route('/Bugs', methods=['DELETE'])
+@app.route('/DB', methods=['DELETE'])
 def DELETE():
     SQL.Db().delete_user(request.json['id'])
+
+@app.route('/json', methods=['GET'])
+def GET_JSON():
+    with open(f'D:/BugsV2/back/JSON/{request.json["who"]}.json', 'r', encoding='utf-8') as file:
+        new_file = file.read()
+        file.close()
+    return json.loads(new_file)
+
+@app.route('/json', methods=['POST'])
+def POST_JSON():
+    with open(f'D:/BugsV2/back/JSON/{list(request.get_json().keys())[0]}.json', 'w', encoding='utf-8') as file:
+        json.dump(request.get_json().get(list(request.get_json().keys())[0]), file)
+        file.close()
+        return "Выполнено"
+
 
 app.run(debug=True)
